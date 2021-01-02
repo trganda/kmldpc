32fbd96739462a5f6342bf1759088781621c423e
trganda <jongsx1203@gmail.com>
2021-01-02 19:49:38 +0800

    add time counter

--- kmldpc/kmldpc.cpp
+++ kmldpc/kmldpc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "randnum.h"
 #include "log.h"
@@ -6,6 +7,9 @@
 #include "ldpclinearsystem.h"
 
 int main() {
+    // Start measuring time
+    auto begin = std::chrono::high_resolution_clock::now();
+
     std::string dirname = "logs";
     std::string logFileName = lab::logger::Log::get_time() + "-kmldpc.logger";
     std::ofstream logFile(dirname + "/" + logFileName);
@@ -31,5 +35,21 @@ int main() {
         lab::logger::ERROR("Encouter error while opening config.toml", true);
     }
     lab::logger::INFO("Simulation done", true);
+
+    // Stop measuring time and calculate the elapsed time
+    auto end = std::chrono::high_resolution_clock::now();
+    auto total_time_minutes = std::chrono::duration_cast<std::chrono::minutes>(end - begin).count();
+    auto total_time_seconds = std::chrono::duration_cast<std::chrono::seconds>(end - begin).count();
+    auto total_time_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+
+    total_time_milliseconds -= total_time_minutes * 60 * 1000;
+    total_time_seconds -= total_time_minutes * 60;
+
+    std::stringstream stream;
+    stream << "Total time cost: "
+           << total_time_minutes << "min:"
+           << total_time_seconds << "sec:"
+           << total_time_milliseconds << "ms";
+    lab::logger::INFO(stream.str(), true);
     return 0;
 }
