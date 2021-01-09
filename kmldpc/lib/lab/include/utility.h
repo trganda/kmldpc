95796a787f43fd2ed76bf46c2620a18e10a160df
trganda <jongsx1203@gmail.com>
2021-01-09 18:46:23 +0800

    added gray qpsk constellation file

--- kmldpc/lib/lab/include/utility.h
+++ kmldpc/lib/lab/include/utility.h
@@ -8,11 +8,17 @@
 
 namespace lab {
 const double kPi = 3.14159265358979;
+
 const double kSmallestProb = 1.0e-12;
+
 const double kMaxDouble = 1.0e+300;
+
 const double kSqrt2 = 1.4142135623730950488016;
+
 const double kSmallestLLR = -28.0;
+
 const double kLargestLLR = 28.0;
+
 // Tanner Edge for LDPC
 typedef struct Edge {
   int m_row_no;
@@ -26,14 +32,12 @@ typedef struct Edge {
   struct Edge *up;
   struct Edge *down;
 } Edge;
+
 namespace utility {
 // using inline to avoid multi definition of function
-void
-MatrixProd(int *uu, int *cc, int **G, int dim, int len);
-void
-ProbClip(double *xx, int len_xx);
-std::ostream &
-operator<<(std::ostream &os, const std::complex<double> &c);
+void MatrixProd(int *uu, int *cc, int **G, int dim, int len);
+void ProbClip(double *xx, int len_xx);
+std::ostream &operator<<(std::ostream &os, const std::complex<double> &c);
 }// namespace utility
 }// namespace lab
 #endif
